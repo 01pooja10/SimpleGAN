6b4fc3b7f2b694c292883e2c5cd95e717d0e0a3d
01pooja10 <66198904+01pooja10@users.noreply.github.com>
2021-05-17 12:54:50 +0530

    Changed training.py
    Changed training.py

--- code/training.py
+++ code/training.py
@@ -9,6 +9,8 @@ from model import Discriminator, Generator
 from torch.optim import Adam
 from torch.utils.tensorboard import SummaryWriter
 from torchvision.utils import make_grid
+import warnings
+warnings.filterwarnings('ignore')
 
 #hyperparameters
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -17,7 +19,7 @@ lr = 0.0001
 noise_dim = 128
 in_size = 784
 batch_size = 64
-epochs = 10
+epochs = 70
 
 #initialize disc and gen models
 disc = Discriminator(in_size).to(device)
@@ -29,7 +31,7 @@ def generate_noise(b,n):
 
 #loading data
 noise = generate_noise(batch_size,noise_dim)
-transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.13,),(0.13,))])
+transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,),(0.5,))])
 data = datasets.MNIST(root=r'dataset/',download=False,transform=transform)
 train = DataLoader(data,batch_size=batch_size,shuffle=True)
 
@@ -39,8 +41,8 @@ optim_gen = Adam(gen.parameters(), lr=lr)
 criterion = nn.BCELoss()
 
 #tensorboard
-board_fake = SummaryWriter(f'run/SimpleGAN/fake')
-board_real = SummaryWriter(f'run/SimpleGAN/real')
+board_fake = SummaryWriter(f'run/fake')
+board_real = SummaryWriter(f'run/real')
 step = 0
 
 print('Starting the training process: ')
@@ -70,7 +72,7 @@ for epoch in range(epochs):
 
         #print loss values
         if idx == 0:
-            print('Epoch: ',epoch,'Batch: ',idx,'Discriminator Loss: ',dloss,'Generator Loss: ',gloss)
+            print('Epoch: ',epoch,'Discriminator Loss: ',dloss.item(),'Generator Loss: ',gloss.item())
 
             with torch.no_grad():
                 fake = gen(noise).reshape(-1,1,28,28)
@@ -79,7 +81,17 @@ for epoch in range(epochs):
                 real_grid = make_grid(actual,normalize=True)
 
                 board_fake.add_image('MNIST FAKE IMAGES',fake_grid,global_step=step)
-                board_real.add_image('MNIST REAL IMAGES',real_grid,gloabl_step=step)
+                board_real.add_image('MNIST REAL IMAGES',real_grid,global_step=step)
 
-                step+=1
+                step = step + 1
+
+print('Final loss for D: ',dloss)
+print('Final loss for G: ',gloss)
 print('Training complete.')
+
+
+d_save = 'disc_model.pth'
+torch.save(disc.state_dict(), d_save)
+
+g_save = 'gen_model.pth'
+torch.save(gen.state_dict(), g_save)
